6c22db54110e276268d6046fdfcceee1e0ac59eb
Luis Cardenas <luis.cardenas@dtone.com>
2020-04-13 22:50:40 -0400

    Chapter19 - Nesting Components

---
+++ ninja_tutorial/chapter19/src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+  import Ninjas from './NinjasApp.vue'
+
+  export default {
+      components: {
+        'ninjas': Ninjas
+      },
+      data () {
+        return {
+          title: 'Welcome to Your Vue.js App'
+        }
+      }
+  }
+</script>
+
+<style>
+
+</style>
